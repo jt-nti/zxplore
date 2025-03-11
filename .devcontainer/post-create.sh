70cfcc94de01eb377fc2b153287f8a7010d1e7b0
James Taylor <jamest@uk.ibm.com>
2025-03-11 16:37:54 +0000

    Create zowe team configuration
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -16,3 +16,14 @@ npm install -g prebuild-install
 npm install -g @zowe/cli@zowe-v3-lts
 
 zowe plugins install @zowe/cics-for-zowe-cli@zowe-v3-lts @zowe/zos-ftp-for-zowe-cli@zowe-v3-lts
+
+#
+# Set up the zowe team configuration file
+#
+zowe config init --global-config --overwrite --for-sure
+zowe config set "autoStore" "false" --global-config
+zowe config set "profiles.global_base.secure" "[]" --json --global-config
+zowe config set "profiles.global_base.properties.host" "204.90.115.200" --global-config
+zowe config set "profiles.global_base.properties.rejectUnauthorized" "false" --global-config
+zowe config set "profiles.zosmf.properties.port" "10443" --global-config
+zowe config set "profiles.tso.properties.account" "FB3" --global-config
