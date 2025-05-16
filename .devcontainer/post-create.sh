b6167906b664fc054122b00b36fb87eed3effd3f
James Taylor <jamest@uk.ibm.com>
2025-05-16 15:59:00 +0100

    Fix secure credential storage
    Unlock the gnome keyring daemon with a random password inside the
    dev container
    
    For more info, see https://github.com/jt-nti/dev-container-keyring
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -20,9 +20,7 @@ zowe plugins install @zowe/zos-ftp-for-zowe-cli@zowe-v3-lts
 #
 # Set up the zowe team configuration file
 #
-zowe config init --global-config --overwrite --for-sure
-zowe config set "autoStore" "false" --global-config
-zowe config set "profiles.global_base.secure" "[]" --json --global-config
+zowe config init --global-config --overwrite --for-sure --prompt false
 zowe config set "profiles.global_base.properties.host" "204.90.115.200" --global-config
 zowe config set "profiles.global_base.properties.rejectUnauthorized" "false" --global-config
 zowe config set "profiles.zosmf.properties.port" "10443" --global-config
