f816a6f0fec839c1bf12b0db9ae2ad9e8926d306
James Taylor <jamest@uk.ibm.com>
2025-07-01 12:13:16 +0100

    Clarify zowe config comments
    Includes a link to the gory details of how all the zowe config files
    get merged
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -18,9 +18,14 @@ npm install -g @zowe/cli@zowe-v3-lts
 zowe plugins install @zowe/zos-ftp-for-zowe-cli@zowe-v3-lts
 
 #
-# Set up the zowe user configuration file
+# Set up the global zowe user configuration file
+#
+# For more information about all the different Zowe config files and
+# how they're merged, see
+# https://github.com/zowe/zowe-cli/blob/master/docs/How_config_files_are_merged.md
 #
 zowe config init --global-config --user-config --overwrite --for-sure --prompt false
 zowe config set "profiles" "{}" --global-config --user-config --json
 export ZXPLORE_PROFILE=$(jq -c . .devcontainer/zxplore.profile.json) && zowe config set "profiles.zxplore" "$ZXPLORE_PROFILE" --global-config --user-config --json
 zowe config set "defaults" '{"zosmf":"zxplore.zosmf","tso":"zxplore.tso","ssh":"zxplore.ssh"}' --global-config --user-config --json
+zowe config list --locations
