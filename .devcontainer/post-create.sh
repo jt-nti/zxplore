2459134050f5189e565096179c876f219e685c23
James Taylor <jamest@uk.ibm.com>
2025-08-14 17:46:03 +0000

    Add Zowe project team profile
    I _think_ this makes more sense than generating a global team profile
    
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -18,14 +18,11 @@ npm install -g @zowe/cli@zowe-v3-lts
 zowe plugins install @zowe/zos-ftp-for-zowe-cli@zowe-v3-lts
 
 #
-# Set up the global zowe user configuration file
+# Set up JSON schema for the Zowe project team configuration file
 #
 # For more information about all the different Zowe config files and
 # how they're merged, see
 # https://github.com/zowe/zowe-cli/blob/master/docs/How_config_files_are_merged.md
 #
-zowe config init --global-config --user-config --overwrite --for-sure --prompt false
-zowe config set "profiles" "{}" --global-config --user-config --json
-export ZXPLORE_PROFILE=$(jq -c . .devcontainer/zxplore.profile.json) && zowe config set "profiles.zxplore" "$ZXPLORE_PROFILE" --global-config --user-config --json
-zowe config set "defaults" '{"zosmf":"zxplore.zosmf","tso":"zxplore.tso","ssh":"zxplore.ssh"}' --global-config --user-config --json
-zowe config list --locations
+zowe config schema > zowe.schema.json
+zowe config list
--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Get connected to the IBM Z Xplore learning platform with a Visual Studio Code de
 
 Includes the Zowe CLI and Zowe Explorer VS Code extension, along with pre-configured IBM Z Xplore connection profiles.
 
+If you have a Z Xplore username and password, run the following command in the dev container to check you can connect.
+
+```shell
+zowe files list data-set ZXP.PUBLIC.*
+```
+
 For more information, see:
 
 - [IBM Z Xplore](https://www.ibm.com/z/resources/zxplore)
